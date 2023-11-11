a16875d2f0bfb8d5515f2754f0c6568e746e7fa5
Humberto Luciano <humberto_luciano@hotmail.com>
2023-11-11 13:06:27 -0300

    feat: Navigation bar (Admin)

--- app/(dashboard)/[storeid]/(routes)/page.tsx
+++ app/(dashboard)/[storeid]/(routes)/page.tsx
@@ -1,20 +1,23 @@
+import prismadb from "@/lib/prismadb";
+
+
 interface DashboardPageProps {
-    params: {storeId: string}
+  params: { 
+    storeId: string 
+};
 }
 
-const DashboardPage: React.FC<DashboardPageProps> = async ({
-    params
-}) => {
-    const store = await prismadb.store.findFirst({
-        whrere: {
-            id: params.storeId
-        }
-    })
-    return (
-        <div>
-            Active Store: ${store?.name};
-        </div>
-    )
-}
+const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+    },
+  });
+  return (
+    <div>
+      Active Store: {store?.name}
+    </div>
+  )
+};
 
 export default DashboardPage;
